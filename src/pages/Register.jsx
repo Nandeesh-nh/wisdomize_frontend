1a89e4f0d3444e27475423f3dbf2658aeb26c0bf
Nandeesh-nh <nhbrothers123@gmail.com>
2025-07-29 19:43:57 +0530

    corrected padding

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -35,7 +35,7 @@ export default function Register() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-950 text-white">
+    <div className="mt-4 mb-4 min-h-screen flex items-center justify-center bg-gray-950 text-white">
       <div className="bg-gray-900 p-8 rounded-2xl shadow-lg w-full max-w-md">
         <h2 className="text-3xl font-bold text-center mb-6 text-indigo-400">Create an Account</h2>
         
