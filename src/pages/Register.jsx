f8ee1cc7113828ad2f1ec1e5b3effe769825157c
Nandeesh-nh <nhbrothers123@gmail.com>
2025-07-29 21:04:08 +0530

    deploy final

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -35,8 +35,8 @@ export default function Register() {
   };
 
   return (
-    <div className="mt-4 mb-4 min-h-screen flex items-center justify-center bg-gray-950 text-white">
-      <div className="bg-gray-900 p-8 rounded-2xl shadow-lg w-full max-w-md">
+    <div className=" min-h-screen flex items-center justify-center bg-gray-950 text-white">
+      <div className="mt-6 mb-6 bg-gray-900 p-8 rounded-2xl shadow-lg w-full max-w-md">
         <h2 className="text-3xl font-bold text-center mb-6 text-indigo-400">Create an Account</h2>
         
         {error && <p className="text-red-500 text-center mb-4">{error}</p>}
